0fe1900bdd2a3a361de88fe2c986a2afac4f2a87
PavelSilviu <pavelsilviu926@gmail.com>
2022-11-07 13:46:53 +0200

    added Payment model

---
+++ DoctorAppointment/DoctorAppointment.Domain/Models/Payment.cs
@@ -0,0 +1,14 @@
+﻿using DoctorAppointment.Domain.Models.Request;
+
+namespace DoctorAppointment.Domain.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; private set; }
+
+        public int Amount { get; private set; }
+        public User Patient { get; private set; }
+
+    }
+}
