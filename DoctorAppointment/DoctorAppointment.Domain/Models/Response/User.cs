5b4513bb3fbc4dfe785b607f6ebdc37f5bc82ac2
vitalie7bocicov <vitalie.bocicov99@gmail.com>
2022-11-07 09:50:09 +0200

    medicine and bill added

--- DoctorAppointment/DoctorAppointment.Domain/Models/Response/User.cs
+++ DoctorAppointment/DoctorAppointment.Domain/Models/Response/User.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DoctorAppointment.Domain.Models.Response
+﻿namespace DoctorAppointment.Domain.Models.Response
 {
     public class User
     {
