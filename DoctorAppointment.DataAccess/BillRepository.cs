9ac8c565602cabca749d675b1de802c943d10d09
vitalie7bocicov <vitalie.bocicov99@gmail.com>
2022-11-07 15:12:17 +0200

    bill and medicine models added

---
+++ DoctorAppointment.DataAccess/BillRepository.cs
@@ -0,0 +1,13 @@
+﻿using DoctorAppointment.Domain.Interfaces;
+using DoctorAppointment.Domain.Models.Request;
+
+namespace DoctorAppointment.DataAccess
+{
+    public class BillRepository : IBillRepository
+    {
+        public Guid AddBill(Bill bill)
+        {
+            return new Guid();
+        }
+    }
+}
---
+++ DoctorAppointment.DataAccess/MedicineRepository.cs
@@ -0,0 +1,13 @@
+﻿using DoctorAppointment.Domain.Interfaces;
+using DoctorAppointment.Domain.Models.Request;
+
+namespace DoctorAppointment.DataAccess
+{
+    public class MedicineRepository : IMedicineRepository
+    {
+        public Guid AddMedicine(Medicine medicine)
+        {
+            return new Guid();
+        }
+    }
+}
